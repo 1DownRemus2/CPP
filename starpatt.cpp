58d79489a75ea2a758b28e9a59a67cf149f2ad9d
Omkarnath Parui <omkarnathparui@Omkarnaths-MacBook-Pro.local>
2025-08-09 00:34:42 +0530

    Meow

---
+++ starpatt.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int num=0;
+    cout<<"Enter the number of rows: ";
+    cin>>num;
+
+    for(int i=0;i<num;i++){
+        cout<<"I lobe you Tanu"<<endl;
+
+    }
+
+    return 0;
+    
+}
